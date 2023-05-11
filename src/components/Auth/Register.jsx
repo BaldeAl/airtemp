c1c1b301870f64999489985022e56f58d0da59cf
Balde <alseinybalde1@gmail.com>
2023-05-11 13:43:07 +0200

    finalisation des fonctionnalités demander Changes to be committed: 	modified:   src/components/Auth/Register.jsx
    Changes not staged for commit:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   prisma/dev.db
    	modified:   src/components/Auth/Login.jsx
    	modified:   src/components/Auth/Update.jsx
    	modified:   src/components/Layout.jsx
    	modified:   src/components/loading/Loading.jsx
    	modified:   src/components/navbar/Navbar.jsx
    	modified:   src/components/place/Place.jsx
    	modified:   src/components/place/PlaceDetails.jsx
    	modified:   src/components/place/Places.jsx
    	modified:   src/pages/_app.js
    	modified:   src/pages/api/auth/register.ts
    	modified:   src/pages/index.js
    
    Untracked files:
    	src/components/Welcome.jsx
    	src/pages/404.jsx

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useRouter } from 'next/router';
 import Link from "next/link"
+import {RiErrorWarningFill} from "react-icons/ri"
 
 const Register = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const router = useRouter();
-    const [message, steMessage] = useState("");
+    const [message, setMessage] = useState("");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -22,16 +23,19 @@ const Register = () => {
             const data = await response.json()
         if (response.ok) {
             localStorage.setItem('token', data.token);
+            localStorage.setItem('UserName', name);
             router.push("/");
         } else {
-            // handle error
-            steMessage("ce n'est pas bon du tout")
+            if (response.status === 409) {
+            setMessage("Email already in use");  // Display a specific error message for duplicate email
+        } else {
+            setMessage("An error occurred");  // Generic error message for all other errors
+        }
         }
     };
 
     return ( 
         <div className="flex min-h-[calc(100vh-100px)] flex-col max-w-7xl mx-auto px-4">
-            {message && <div>{message}</div>}
         <div className="flex-1 flex-grow">
             <h2 className="text-2xl font-bold mb-4">
                 Register
@@ -75,6 +79,14 @@ const Register = () => {
             <Link className="text-sm font-medium text-gray-500"
              href={`/Auth/login/`}>Already have an account? Login
             </Link>
+            {message && 
+                <div className="alert text-red-500">
+                    <RiErrorWarningFill className="w-5 h-5 inline mr-3"/>
+                <span className="font-medium">
+                        {message}
+                </span>
+                </div>
+                }
         </div>
         </div>
      );
